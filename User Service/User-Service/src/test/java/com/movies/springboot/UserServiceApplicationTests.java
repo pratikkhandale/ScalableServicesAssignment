8aae87a0f9a70ba11223426d6401478e682bb813
AT092729 <Anusua.Thakur@cerner.com>
2024-10-28 21:09:46 +0530

    added user service

---
+++ User Service/User-Service/src/test/java/com/movies/springboot/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
